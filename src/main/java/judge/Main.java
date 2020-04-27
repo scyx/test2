44005c4bb7edf69743d979d1b331afee4c2f60c1
cyx <774169864@qq.com>
2020-04-27 19:45:35 +0800

    test

---
+++ src/main/java/judge/Main.java
@@ -0,0 +1,13 @@
+package judge;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author cyx
+ * @data 2020/4/27 19:39
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println(StringUtils.isNotBlank("123"));
+    }
+}
