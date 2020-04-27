20c167cd657932ae8ee572b814c1005d99080dd7
cyx <38756206+scyx@users.noreply.github.com>
2020-04-27 19:47:21 +0800

    update

--- src/main/java/judge/Main.java
+++ src/main/java/judge/Main.java
@@ -8,6 +8,6 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println(StringUtils.isNotBlank("123"));
+        System.out.println(StringUtils.isNotBlank("123"));//123
     }
 }
